1df50acaea93a79d516852911651513f35ceb83b
Cooper Saye <coopersaye@gmail.com>
2021-04-17 16:55:08 -0600

    Create Player class.

---
+++ InfiniteExplore/Player.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace InfiniteExplore
+{
+    public class Player
+    {
+        private Vector2 position;
+        private Vector2 size;
+
+        private Vector2 direction;
+
+        private Rectangle Bounds
+        {
+            get { return new Rectangle(position.ToPoint(), size.ToPoint()); }
+        }
+
+        private const float Speed = 1;
+        private readonly Vector2 SpriteSize = new Vector2(16, 32);
+
+        public Vector2 Position => position;
+        public Vector2 Size => size;
+
+        public Player(int x, int y, int w, int h)
+        {
+            position = new Vector2(x, y);
+            size = new Vector2(w, h);
+        }
+
+        public void Update(float delta, Game1 game)
+        {
+            // Get keyboard state
+            KeyboardState state = game.KeyboardState;
+
+            // Get movement direction
+            direction.X = state.IsKeyDown(Keys.A) ? -1 : state.IsKeyDown(Keys.D) ? 1 : 0;
+            direction.Y = state.IsKeyDown(Keys.W) ? -1 : state.IsKeyDown(Keys.S) ? 1 : 0;
+
+            // Update position by movement direction
+            position += direction * Speed * delta;
+        }
+
+        public void Draw(Game1 game)
+        {
+            Drawing.DrawSprite(Drawing.PlayerTexture, Bounds, SpriteSize, 0, game);
+        }
+    }
+}
